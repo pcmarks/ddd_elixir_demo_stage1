316f57e707ad24402c979c8ce5d1e9ae77a7a8d4
pcmarks <peter.c.marks@gmail.com>
2017-11-03 10:38:11 -0400

    Remove New function from Clerk UI
    Also, remove supporting functions in Phoenix

--- apps/shipping_web/lib/shipping_web/controllers/handling_event_controller.ex
+++ apps/shipping_web/lib/shipping_web/controllers/handling_event_controller.ex
@@ -1,4 +1,10 @@
 defmodule ShippingWeb.HandlingEventController do
+  @moduledoc """
+  Support for displaying Handling Events, primarily for the Shipping Clersk.
+
+  NOTE: Commented out code can be used to create a new Handling Event for
+  future demo stages.
+  """
   use ShippingWeb,  :controller
 
   # The Aggregates
@@ -10,33 +16,33 @@ defmodule ShippingWeb.HandlingEventController do
     handling_events = HandlingEvents.list_handling_events()
     render(conn, :index, handling_events: handling_events)
   end
-  def new(conn, _params) do
-    changeset = HandlingEvents.change_handling_event(%HandlingEvents.HandlingEvent{})
-    render(conn, :new, changeset: changeset,
-                          location_map: Locations.location_map(),
-                        handling_event_type_map: Cargoes.handling_event_type_map())
-  end
-
-  def create(conn, %{"handling_event" => handling_event_params}) do
-    case HandlingEvents.create_handling_event(handling_event_params) do
-      {:ok, handling_event} ->
-        # Let everyone who has subscribed  know about the new handling event.
-        # Determine a cargo's new status given this handling event. Update the cargo
-        # Let everyone who has subscribed know the change in the cargo's status.
-        # The tracking status is ignored for now.
-        {tracking_status, updated_cargo} =
-          handling_event
-          |> HandlingEventChannel.broadcast_new_handling_event()
-          |> Cargoes.update_cargo_status()
-          |> HandlingEventChannel.broadcast_new_cargo_status()
-        conn
-          |> put_flash(:info, "Handling event created successfully.")
-          |> redirect(to: handling_event_path(conn, :index))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset,
-                                location_map: Locations.location_map(),
-                                handling_event_type_map: Cargoes.handling_event_type_map())
-    end
-  end
+  # def new(conn, _params) do
+  #   changeset = HandlingEvents.change_handling_event(%HandlingEvents.HandlingEvent{})
+  #   render(conn, :new, changeset: changeset,
+  #                         location_map: Locations.location_map(),
+  #                       handling_event_type_map: Cargoes.handling_event_type_map())
+  # end
+  #
+  # def create(conn, %{"handling_event" => handling_event_params}) do
+  #   case HandlingEvents.create_handling_event(handling_event_params) do
+  #     {:ok, handling_event} ->
+  #       # Let everyone who has subscribed  know about the new handling event.
+  #       # Determine a cargo's new status given this handling event. Update the cargo
+  #       # Let everyone who has subscribed know the change in the cargo's status.
+  #       # The tracking status is ignored for now.
+  #       {tracking_status, updated_cargo} =
+  #         handling_event
+  #         |> HandlingEventChannel.broadcast_new_handling_event()
+  #         |> Cargoes.update_cargo_status()
+  #         |> HandlingEventChannel.broadcast_new_cargo_status()
+  #       conn
+  #         |> put_flash(:info, "Handling event created successfully.")
+  #         |> redirect(to: handling_event_path(conn, :index))
+  #     {:error, %Ecto.Changeset{} = changeset} ->
+  #       render(conn, "new.html", changeset: changeset,
+  #                               location_map: Locations.location_map(),
+  #                               handling_event_type_map: Cargoes.handling_event_type_map())
+  #   end
+  # end
 
 end
