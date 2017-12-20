76532a4925d96098da22dac9b6bd69957a6fe373
pcmarks <peter.c.marks@gmail.com>
2017-12-20 12:06:01 -0500

    Comment DeliveryHistory

--- apps/shipping/lib/shipping/cargoes/delivery_history.ex
+++ apps/shipping/lib/shipping/cargoes/delivery_history.ex
@@ -1,8 +1,14 @@
 defmodule Shipping.Cargoes.DeliveryHistory do
-
+  @moduledoc """
+  A delivery history for a particular cargo is a list of all of the handling
+  events associated with the cargo.
+  """
   alias Shipping.Repo
   alias Shipping.HandlingEvents.HandlingEvent
 
+  @doc """
+  Retrieve all handling events associated with this tracking id
+  """
   def for_tracking_id(tracking_id) do
     Repo.get_by_tracking_id!(HandlingEvent, tracking_id)
   end
