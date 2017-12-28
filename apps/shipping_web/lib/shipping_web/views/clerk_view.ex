87fafdcba26882b4702e38e3bce1911c6e13d5f5
pcmarks <peter.c.marks@gmail.com>
2017-12-28 16:09:48 -0500

    No need for JSON response for clerk and opsmanager views
    The :index option is not called thus need not return anything to the Elm
    client.

--- apps/shipping_web/lib/shipping_web/views/clerk_view.ex
+++ apps/shipping_web/lib/shipping_web/views/clerk_view.ex
@@ -1,7 +1,12 @@
 defmodule ShippingWeb.ClerkView do
+  @moduledoc """
+  For the Phoenix client - the template clerk/index.html.eex is rendered
+  (by default).
+
+  For the Elm client, there is no rendering (response, i.e., index.json) because
+  the Elm client already contains everything to display the Clerk's HTML.
+
+  """
   use ShippingWeb, :view
 
-  def render "index.json", _params do
-    %{"status": "ok"}
-  end
 end
--- apps/shipping_web/lib/shipping_web/views/ops_manager_view.ex
+++ apps/shipping_web/lib/shipping_web/views/ops_manager_view.ex
@@ -3,8 +3,8 @@ defmodule ShippingWeb.OpsManagerView do
   For the Phoenix client - the template ops_manager/index.html.eex is rendered
   (by default).
 
-  For the Elm client, there is no rendering (response, i.e., index.json) because the Elm client contains
-  everything necessary to display the Ops Manager's HTML.
+  For the Elm client, there is no rendering (response, i.e., index.json) because
+  the Elm client already contains everything to display the Ops Manager's HTML.
 
   """
   use ShippingWeb, :view
