dd19b639bc18013ecf0235e5a116eb8739676a5c
pcmarks <peter.c.marks@gmail.com>
2017-12-29 13:10:17 -0500

    Refactor showing of OpsManager - HandlingEvents

--- apps/shipping_web/lib/shipping_web/controllers/handling_event_controller.ex
+++ apps/shipping_web/lib/shipping_web/controllers/handling_event_controller.ex
@@ -1,9 +1,6 @@
 defmodule ShippingWeb.HandlingEventController do
   @moduledoc """
-  Support for displaying Handling Events, primarily for the Clersk.
-
-  NOTE: Commented out code can be used to create a new Handling Event for
-  future demo stages.
+  Support for displaying Handling Events, primarily for the Operations Manager.
   """
   use ShippingWeb,  :controller
 
@@ -14,16 +11,7 @@ defmodule ShippingWeb.HandlingEventController do
 
   def show(conn, _params) do
     handling_events = HandlingEvents.list_handling_events()
-
-    # NOTE: Because we are employing a single page view for the Clerk page,
-    # we render the ClerkView index page passing a cargo and its handling events
-    case get_format(conn) do
-      "json" ->
-        render(conn, :show, handling_events: handling_events)
-      _ ->
-        render(conn, ShippingWeb.OpsManagerView, :index,
-                    handling_events: handling_events)
-    end
+    render(conn, :show, handling_events: handling_events)
   end
 
 end
